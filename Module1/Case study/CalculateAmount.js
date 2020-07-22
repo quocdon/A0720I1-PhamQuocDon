a1fd7d5b40d926f6204e9f8c9b655cf1f1c016b7
quocdon <quocdon2991@gmail.com>
2020-07-22 23:51:45 +0700

    add case study with booking confirmation

--- Module1/Case study/CalculateAmount.js
+++ Module1/Case study/CalculateAmount.js
@@ -9,13 +9,68 @@ function totalamount() {
     let roomtype = document.forms["guestinfo"]["roomtype"];
     let price;
     let amount;
+    let discountprice;
     if (services.value=="Villa"){price=500}
     else{
         if(services.value=="House"){price=300}
         else price=100
     }
-    amount = rentdays.value * price *(1-10/100);
-    document.getElementById("ketqua").innerHTML = "Tên khách hàng: "+ guestname.value +"<br>" +
+    discountprice = price*(1-10/100)
+    amount = rentdays.value * discountprice;
+    document.getElementById("ketqua").innerHTML = "<table class='confirmationTable'>" +
+        "<tr>" +
+            "<td colspan='2' style='text-align: center'>XÁC NHẬN ĐẶT PHÒNG/ BOOKING CONFIRMATION</td>"+
+        "</tr>"+
+        "<tr>" +
+            "<td>Tên khách hàng/ Guest name: </td>"+
+            "<td>" + guestname.value +"</td>"+
+        "</tr>"+
+        "<tr>"+
+            "<td>Số CMND/ID Number: </td>"+
+            "<td>" + guestid.value + "</td>"+
+        "</tr>"+
+        "<tr>"+
+            "<td>Ngày sinh: </td>"+
+            "<td>" + guestdob.value + "</td>"+
+        "</tr>"+
+        "<tr>"+
+            "<td>Địa chỉ: </td>"+
+            "<td>" + address.value + "</td>"+
+        "</tr>"+
+        "<tr>"+
+            "<td>Hạng khách hàng/ Customer class: </td>"+
+            "<td>" + guestclass.value + "</td>"+
+        "</tr>"+
+        "<tr>"+
+            "<td>Số ngày thuê/ Rentdays: </td>"+
+            "<td>" + rentdays.value + "</td>"+
+        "</tr>"+
+        "<tr>"+
+            "<td>Loại dịch vụ/ Services: </td>"+
+            "<td>" + services.value + "</td>"+
+        "</tr>"+
+        "<tr>"+
+            "<td>Loại phòng/ Room type: </td>"+
+            "<td>" + roomtype.value + "</td>"+
+        "</tr>"+
+        "<tr>"+
+            "<td>Đơn giá/ Price: </td>"+
+            "<td>"+ discountprice + " USD/đêm - (Giá gốc: "+ price + " USD/Đêm)" + "</td>"+
+        "</tr>"+
+        "<tr>"+
+            "<td>Thành tiền/ Total Amount: </td>"+
+            "<td style='color: red'>" + amount + "  USD</td>"+
+        "</tr>"+
+        "</table>"
+
+
+
+
+
+
+
+    /*"<p class='gueststyle'>XÁC NHẬN ĐẶT PHÒNG/ BOOKING CONFIRMATION</p>"+ "<br>"+
+        "Tên khách hàng: "+ guestname.value +"<br>" +
         "Số CMND: " + guestid.value +"<br>"+
         "Ngày Sinh: " + guestdob.value +"<br>"+
         "Địa chỉ: " + address.value + "<br>"+
@@ -23,6 +78,6 @@ function totalamount() {
         "Số ngày thuê: " + rentdays.value +"<br>"+
         "Dịch vụ thuê: " + services.value +"<br>"+
         "Loại phòng: " + roomtype.value + "<br>"+
-        "Giá tiền: " + amount +"VND";
+        "Giá tiền: " + amount +"VND";*/
 
 }
