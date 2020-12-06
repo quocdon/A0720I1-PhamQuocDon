e674d98027e0215c386cbff0613b4a716d27a578
quocdon <quocdon2991@gmail.com>
2020-12-07 01:35:37 +0700

    case study task 1

--- Module3/_99_case_study/task_01/task_01_SQL_script.sql
+++ Module3/_99_case_study/task_01/task_01_SQL_script.sql
@@ -56,6 +56,8 @@ foreign key (IDViTri) references ViTri(IDViTri),
 foreign key (IDTrinhDo) references TrinhDo(IDTrinhDo),
 foreign key (IDBoPhan) references BoPhan(IDBoPhan)
 );
+alter table NhanVien
+modify Luong int not null;
 create table KhachHang(
 IDKhachHang int not null auto_increment,
 IDLoaiKhach int not null,
@@ -68,6 +70,8 @@ DiaChi varchar(45) not null,
 constraint KhachHang_pk primary key (IDKhachHang),
 foreign key (IDLoaiKhach) references LoaiKhach(IDLoaiKhach)
 );
+alter table KhachHang
+modify NgaySinh date not null;
 create table DichVu(
 IDDichVu int not null auto_increment,
 TenDichVu varchar(45) not null unique,
@@ -136,3 +140,34 @@ insert into KieuThue(TenKieuThue) values ('Gio');
 insert into LoaiDichVu(TenLoaiDichVu) values('Villa');
 insert into LoaiDichVu(TenLoaiDichVu) values('House');
 insert into LoaiDichVu(TenLoaiDichVu) values('Room');
+insert into KhachHang values 
+(1, 2, 'Nguyen Van An', '1991-11-22', '111222333', '0931552555', 'nguyenan1991@gmail.com', 'Da Nang'),
+(2, 1, 'Pham Van Cuong', '1999-12-22', '123255999', '0934333555', 'phamcuong1999@gmail.com', 'Hue'),
+(3, 3, 'Nguyen Hoang Yen', '1994-05-22', '111312444', '0375111555', 'nguyenyen1994@gmail.com', 'Quang Nam'),
+(4, 2, 'Nguyen Thuy Van', '1995-01-26', '111123656', '0364666999', 'nguyenvan1995@gmail.com', 'Da Nang'),
+(5, 4, 'Hoang Van Binh', '1994-04-24', '123000333', '0934696969', 'hoangbinh1994@gmail.com', 'Da Nang'),
+(6, 5, 'Trinh Thanh Giang', '1992-01-12', '100055523', '0934255255', 'trinhgiang1992@gmail.com', 'Hue');
+insert into KhachHang values 
+(7, 2, 'Nguyen Hoang Nam', '1943-10-22', '111635333', '0931552123', 'nguyennam1943@gmail.com', 'Da Nang'),
+(8, 4, 'Pham Quoc Khanh', '2009-12-22', '123125999', '0985252252', 'phamkhanh2009@gmail.com', 'Quang Tri'),
+(9, 3, 'Nguyen Bich Thuy', '1984-06-12', '111000444', '0948248248', 'nguyenthuy1984@gmail.com', 'Quang Tri');
+insert into NhanVien values
+(1, 'Hoang Nguyen Thu Thuyen', 1, 1, 2, '2004-12-31', '1972525252', 5000000, '0375223252','ThuHuyen@furama.com', 'Quang Binh'),
+(2, 'Hoang Van Kien', 6, 4, 4, '1973-01-30', '1114222333', 60000000, '0374555333', 'vankien@furama.vn','Da Nang'),
+(3, 'Pham Thi Thanh', 2, 1, 1, '1994-01-12', '1852222333', 4000000, '0321112252', 'thithanh@furama.vn','Da Nang'),
+(4, 'Tran Van An', 3, 2, 2, '1984-12-15', '1456111333', 8000000, '0395755212', 'vanan@furama.vn','Hue'),
+(5, 'Khuat Thu Giang', 2, 1, 2, '1993-02-25', '1452000333', 12000000, '0395123455', 'thugiang@furama.vn','Binh Dinh'),
+(6, 'Nguyen Van Nam', 5, 2, 4, '2001-04-30', '1992111333', 25000000, '0394555252', 'vannam@furama.vn','Hue'),
+(7, 'Duong Thi Thu', 4, 4, 3, '1999-12-31', '1972145444', 10000000, '0381693222', 'thithu@furama.vn','Quang Nam');
+insert into DichVu values
+(1, 'Codegym Villa', 234, 3, 12, 25000000, 3, 1, 'Kha dung'),
+(2, 'Fusion Villa', 234, 1, 6, 14000000, 1, 1, 'Kha dung'),
+(3, 'Open Villa', 234, 2, 7, 25000000, 2, 1, 'Khong kha dung'),
+(4, 'Funny House', 100, 2, 4, 15000000, 3, 2, 'Kha dung'),
+(5, 'Nightmare House', 110, 1, 3, 8000000, 2, 2, 'Kha dung'),
+(6, 'Rose House', 100, 2, 4, 400000, 3, 2, 'Kha dung'),
+(7, 'Happy Room', 78, 1, 3, 200000, 4, 2, 'Kha dung'),
+(8, 'Crazy Room', 50, 1, 2, 1000000, 3, 2, 'Kha dung');
+update DichVu set IDLoaiDichVu = 3 where IDDichVu >= 7;
+update DichVu set DienTich = 150 where IDDichVu in (2,3);
+
